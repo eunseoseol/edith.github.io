f543348281911682bda2814244ee7b4e13640456
Eunseo Seol <147463468+eunseoseol@users.noreply.github.com>
2024-06-10 22:36:29 +0900

    Update page.js

--- app/page.js
+++ app/page.js
@@ -74,17 +74,7 @@ export default function Home() {
 
   return (
 
-    <Head>
-        <title>Home Page - 스타트업 창업가 커뮤니티</title>
-        <meta property="og:title" content="Home Page - 스타트업 창업가 커뮤니티" />
-        <meta property="og:description" content="Made by Eunseo Seol" />
-        <meta property="og:image" content="https://photos5.appleinsider.com/gallery/product_pages/189-hero.jpg" />
-        <meta property="og:url" content="https://edith3141.com" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Home Page - Your Site Title" />
-        <meta name="twitter:description" content="Description of your home page" />
-        <meta name="twitter:image" content="https://example.com/home-page-image.jpg" />
-      </Head>
+ 
     <main className="p-4 flex flex-col lg:flex-row">
       {/* Articles Section */}
       
@@ -122,32 +112,11 @@ export default function Home() {
       </div>
 
       {/* User Suggestions Section */}
-      <div className="w-full lg:w-1/4 p-4 mb-8 lg:mb-0 lg:ml-4">
-        <h2 className="text-lg font-semibold mb-4">Who to follow</h2>
-        <div className="overflow-x-auto">
-          <div className="flex space-x-4">
-            {users.map(user => (
-              <div key={user.id} className="flex-shrink-0 border p-4 rounded-lg w-48 flex flex-col items-center">
-                {user.profileImage ? (
-                  <img
-                    src={user.profileImage}
-                    alt="Profile"
-                    className="w-12 h-12 rounded-full mb-2"
-                  />
-                ) : (
-                  <div className="w-12 h-12 rounded-full bg-gray-500 text-white flex items-center justify-center mb-2">
-                    {user.name.slice(0, 2)}
-                  </div>
-                )}
-                <div className="text-center mb-2">
-                  <p className="font-bold">{user.name}</p>
-                </div>
-                <button className="bg-blue-500 text-white text-sm px-3 py-1 rounded-full">Follow</button>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+
+ 
+
+
+              
     </main>
   );
 }
